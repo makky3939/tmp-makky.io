8f78ab0700ea16bc403cf26f20e5995464ca3f40
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2015-09-06 13:04:02 +0900

    update

---
+++ root.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# add npm package
+rpm -ivh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
+
+# update epel-release
+yum remove -y epel-release
+yum install -y epel-release
+
+# install docker
+yum install -y docker-io
+
+# install nginx
+yum install -y nginx --enablerepo=nginx
+
+# install ruby
+yum install -y ruby ruby-devel gcc make
+
+# open port 80
+firewall-cmd --zone=public --add-port=80/tcp --permanent
+firewall-cmd --reload
+
+# create user
+adduser makky
+passwd makky
+
+# add group
+gpasswd -a makky wheel
+sed "s/# %wheel        ALL=(ALL)       NOPASSWD: ALL/%wheel        ALL=(ALL)       NOPASSWD: ALL/g" /etc/sudoers
