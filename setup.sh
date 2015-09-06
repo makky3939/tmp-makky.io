2ac690aceb4e0552e9df940dcb6462b15eefaf30
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2015-09-06 11:56:44 +0900

    update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
+# add npm package
+rpm -ivh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
+
+# update epel-release
+yum remove -y epel-release
+yum install -y epel-release
+
+# install docker
+yum install -y docker-io
+
+# install nginx
+yum install -y nginx --enablerepo=nginx
+
+# install ruby
+yum install -y ruby ruby-devel gcc make
+
+# open port 80
+firewall-cmd --zone=public --add-port=80/tcp --permanent
+firewall-cmd --reload
+
 # set service
 systemctl start docker.service
 systemctl enable docker.service
